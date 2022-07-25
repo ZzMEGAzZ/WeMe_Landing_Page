4873239740b1560b1c26a0bb09d2a6ee98fcbfdc
ZzMEGAzZ <apisitsangkrajang@gmail.com>
2022-07-25 10:26:06 +0700

    edit age validate()

--- src/Subscribe.svelte
+++ src/Subscribe.svelte
@@ -3,7 +3,7 @@
     function validate() {
         var name_v = document.forms["my-Form"]["name"].value;
         var last_v = document.forms["my-Form"]["lastname"].value;
-        var age_v = document.forms["my-Form"]["age"].value;
+        // var age_v = document.forms["my-Form"]["age"].value;
         var email_v = document.forms["my-Form"]["email"].value;
 
         if(name_v =="" || last_v ==""|| age_v ==""|| email_v==""){
